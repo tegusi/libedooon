c795a870c1e3f8bdcc86160c1a2e02495939c967
Lei Yang <yltt1234512@gmail.com>
2016-03-05 11:18:13 +0800

    release to pypi

---
+++ setup.py
@@ -0,0 +1,30 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='libedooon',
+    packages=['libedooon'],
+    version='0.1',
+    description='A Python wrap for Edooon RESTful APIs.',
+    author='Lei Yang',
+    author_email='yltt1234512@gmail.com',
+    url='https://github.com/yangl1996/libedooon',
+    keywords=['api', 'library'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Scientific/Engineering :: GIS'
+    ],
+    license='MIT License',
+    install_requires=['requests', 'gpxpy'],
+)
